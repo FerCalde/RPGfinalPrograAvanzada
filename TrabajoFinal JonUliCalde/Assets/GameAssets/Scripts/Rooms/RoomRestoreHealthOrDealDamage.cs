811184c72302cd29014d214f98da25fa651fa57c
Aginago <jonoleta@gmail.com>
2021-05-29 18:56:50 +0200

    Room scripts

---
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Rooms/RoomRestoreHealthOrDealDamage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rooms
+{
+    public abstract class RoomRestoreHealthOrDealDamage : Room, IRecoverHealth, IRandomizeHealOrDamageRoom
+    {
+        int _healthToRestore;
+
+        public int healthToRestore
+        {
+            get { return _healthToRestore; }
+            set
+            {
+                _healthToRestore = Mathf.Clamp(value, 0, 100);
+            }
+        }
+
+        public void RandomizeHealOrDamageRoom()
+        {
+            int healOrDamage = Random.Range(0, 1);
+
+            if (healOrDamage.Equals(0))
+            {
+
+            }
+
+
+        }
+
+        public void RecoverHealth()
+        {
+            _healthToRestore = CombatController.instance.heroDisplay.characterInstance.hp += healthToRestore;
+            Debug.Log("Player recovers " + healthToRestore);
+        }
+
+
+
+    }
+}
+
