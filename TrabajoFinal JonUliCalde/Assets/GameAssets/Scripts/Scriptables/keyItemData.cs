2e00cf97336ba4c425d75e4b7147710a64e6bb0a
DESKTOP-7TGDIKI\uliro <uliros@live.com>
2021-05-29 19:54:38 +0200

    asdfasfasf

---
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Scriptables/keyItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventarios
+{
+    [CreateAssetMenu(fileName = "Key", menuName = "Items/key", order = 3)]
+    public class KeyItemData : ItemData
+    {
+        
+    }
+}
+
