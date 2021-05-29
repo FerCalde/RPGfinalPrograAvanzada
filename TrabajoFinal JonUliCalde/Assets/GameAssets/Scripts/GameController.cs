35e04445254ed651f4919f2efa5bf51bddf7f07c
Calde <chipneidev@gmail.com>
2021-05-29 19:48:02 +0200

    sceneStartLoop

--- TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/GameController.cs
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/GameController.cs
@@ -6,8 +6,11 @@ using Inventarios;
 using UnityEngine.UI;
 
 //Crear una clase que herede de EventArgs con los parámetros enemyWaves y enemiesPerWave;
+[System.Serializable]
 public class StartGameArgs : EventArgs
 {
+    public int salaStage;
+
     public int enemyWaves;
     public int enemiesPerWaves;
 
@@ -28,9 +31,10 @@ public class GameController : MonoBehaviour
     #endregion
 
     public GameObject startPanel;
+    public GameObject salasPanel;
 
-    public InputField wavesIField;
-    public InputField enemiesIField;
+    //public InputField wavesIField;
+    //public InputField enemiesIField;
 
     //Crear evento EventHandler siendo su event args del tipo 
     //creado antes y llamarlo "OnCombatStart"
@@ -38,18 +42,24 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        //startPanel.SetActive(true);
+        startPanel.SetActive(true);
+        salasPanel.SetActive(false);
     }
 
     public void StartGame() {
         //  int waves = int.Parse(wavesIField.text);
         //  int enemies = int.Parse(enemiesIField.text);
-        //startPanel.SetActive(false);
+        startPanel.SetActive(false);
         FindObjectOfType<HeroEquipmentDisplay>().OnStatsChange += CombatController.instance.heroDisplay.ActualiceDisplayData;
 
         //Invocar el evento OnCombatStart enviando los parámetros.
         //OnCombatStart?.Invoke(this, new StartGameArgs(waves, enemies));
         
+    }
+
+    public void StarCombat()
+    {
+        //OnCombatStart?.Invoke(this, new StartGameArgs(waves, enemies));
     }
 
 }
