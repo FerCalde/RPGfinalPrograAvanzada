e753ceeedd2feccd08d38abc1a708891501b1fd8
DESKTOP-7TGDIKI\uliro <uliros@live.com>
2021-06-02 18:23:40 +0200

    añksjldfñalskf

---
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Managers/InfoManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoManager : SingletonTemporal<InfoManager>
+{
+    Text infoText;
+    private void Start()
+    {
+        infoText = GetComponent<Text>();
+    }
+    public void InfoChanger(string t)
+    {
+        infoText.text = t;
+    }
+}
