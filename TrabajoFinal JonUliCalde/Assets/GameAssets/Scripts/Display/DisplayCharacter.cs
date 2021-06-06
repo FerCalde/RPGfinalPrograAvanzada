868b7815797094cffa702e119a26df269481b735
FerCalde <chipneidev@gmail.com>
2021-06-06 18:17:46 +0200

    Display Characterss

---
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Display/DisplayCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+namespace enemyStatusConditions
+{
+
+    public class DisplayCharacter : MonoBehaviour
+    {
+
+        Character _character;
+        [HideInInspector]
+        public Character characterInstance
+        {
+            get { return _character; }
+            set
+            {
+
+                //Suscribir y dar de baja del método ActualiceHp al character
+
+                if (value != null)
+                {
+                    value.OnHpChange += ActualiceHp;
+                    _character = value;
+                }
+                else
+                {
+                    value.OnHpChange -= ActualiceHp;
+                }
+
+            }
+        }
+
+        [SerializeField] Text nameTxt;
+        [SerializeField] Text attackTxt;
+        [SerializeField] Text defenseTxt;
+
+        [SerializeField] Image hp;
+        [SerializeField] float critico;
+
+        public void ActualiceDisplayData()
+        {
+            /*nameTxt.text = characterInstance.characerName.ToString();
+            attackTxt.text = characterInstance.attak.ToString();
+            defenseTxt.text = characterInstance.defense.ToString();*/
+
+            critico = characterInstance.critChance;
+            ActualiceHp(characterInstance.hp);
+        }
+
+        public void ActualiceHp(int value)
+        {
+            float width = (value * 100) / characterInstance.maxHp;
+            //hp.rectTransform.sizeDelta = new Vector2(width, hp.rectTransform.sizeDelta.y);
+            hp.fillAmount = width;
+        }
+    }
+}
