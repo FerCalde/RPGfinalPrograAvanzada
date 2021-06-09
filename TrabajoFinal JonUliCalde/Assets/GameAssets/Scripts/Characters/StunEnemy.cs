e9900480e4d0eda4f84b4b1516c1fac80426d402
DESKTOP-7TGDIKI\uliro <uliros@live.com>
2021-06-09 02:03:13 +0200

    enemigos arreglados

--- TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Characters/StunEnemy.cs
+++ TrabajoFinal JonUliCalde/Assets/GameAssets/Scripts/Characters/StunEnemy.cs
@@ -10,20 +10,20 @@ namespace enemyStatusConditions
 
         public void StunAttack(int turns, Character targetToGo)
         {
-            targetToGo.maxPoisonedTurns = turns;
+            targetToGo.maxStunedTurns = turns;
             InfoManager.Instance.InfoChanger(name + " ha stuneado durante " + turns + " turnos.");
         }
         public override void ChoseEnemyAction(int amount, Character targetToGo)
         {
-            float random = Random.Range(0, 1);
+            float random = Random.Range(0, 1.1f);
             if (random > 0.3f)
             {
                 RegularAttack(amount, targetToGo);
             }
             else
             {
-                int turnsPoison = Mathf.RoundToInt(amount / 6);
-                StunAttack(turnsPoison, targetToGo);
+                int turnsStun = Mathf.RoundToInt(amount / 6);
+                StunAttack(turnsStun, targetToGo);
             }
         }
     }
